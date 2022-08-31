1f752257283b84afbe3514d9651e900132b709c1
avazabsamatov <avazabsamatov4@gmail.com>
2022-08-31 10:19:25 +0500

    first commit

---
+++ src/main/java/uz/avaz/code/my_learning_platform/MyLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package uz.avaz.code.my_learning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLearningPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/avaz/code/my_learning_platform/MyLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.avaz.code.my_learning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
